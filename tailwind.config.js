7685fc92c63d8ce8aab00004305161f35710c89f
PranabKumar27 <pranabkumarsahoo27@gmail.com>
2024-08-25 03:54:06 +0530

    First Page of Profile Complete

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'header': ['Quicksand', 'sans-serif'],
+      },
+    },
+  },
+};
