70df6cdb20c6d048aff211f7913fd2276c008913
PranabKumar27 <pranabkumarsahoo27@gmail.com>
2025-04-15 14:44:13 +0530

    perf: improved page reload speed by 1 second

--- JS/BeforePage.js
+++ JS/BeforePage.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
-    // Delay of 5 seconds before hiding the preloader
+
     setTimeout(function () {
         document.getElementById('load-screen-container').style.display = 'none';
         document.getElementById('main-content').style.display = 'block';
-    }, 3000); // 5000 milliseconds = 5 seconds
+    }, 2000); 
 }); 
